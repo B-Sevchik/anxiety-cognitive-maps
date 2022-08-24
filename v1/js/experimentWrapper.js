168682599754d62b6ad2b939d30b2916058af2e1
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2022-08-23 20:52:27 -0400

    incorporate STAI into experiment
    STAI.js was incorporated into experiment.js
    STAI.html was incorporated into index.html
    STAI.css was incorporated into styles.css

--- v1/js/experimentWrapper.js
+++ v1/js/experimentWrapper.js
@@ -10,16 +10,24 @@ function updateMainMenu(expStage){
       $("#soundCheck").hide();
       $("#demographicsForm").show();
       break;
-    case 3: //main task
+    case 3: // STAI questionnaire
       $("#demographicsForm").hide();
       $("#soundCheck").hide();
       $("#table").hide();
+      $("#STAI_div").show();
+      break;
+    case 4: //main task
+      $("#demographicsForm").hide();
+      $("#soundCheck").hide();
+      $("#table").hide();
+      $("#STAI_div").hide();
       startExperiment();
       break;
-    case 4: //debriefing
+    case 5: //debriefing
       $("#mturk_form").show();
       $("#demographicsForm").hide();
       $("#soundCheck").hide();
+      $("#STAI_div").hide();
       $("#table").show();
       break;
   }
@@ -33,6 +41,7 @@ $(document).ready(function(){
   $("#instructions").hide();
   $("#myButton").hide();
   $("#NoGo").hide();
+  $("#STAI_div").hide();
 
   // gets MTurk Worker Information and assign to HTML elements
   let mt = getAllUrlParams(window.location.href);
@@ -109,6 +118,56 @@ function prepareMenu(){
     }
   });
 }
+
+$(document).on("click", "#STAI_submit", function(){
+
+    // code for submit button here
+    var STAI_vals = {}
+    var input_array = document.getElementById('STAI').querySelectorAll('input');
+
+    for (let inputelement = 0; inputelement < input_array.length; inputelement++) {
+      if (input_array[inputelement].checked == true) {
+
+        STAI_vals[input_array[inputelement].name] = input_array[inputelement].value;
+
+      }
+
+    };
+
+    console.log(STAI_vals);
+    console.log(Object.keys(STAI_vals).length);
+    console.log(Object.keys(STAI_vals));
+
+
+//if participant answers all questions
+if (Object.keys(STAI_vals).length == 20) {
+
+  const questions_list_array = document.getElementsByClassName("STAI_question");
+    for (let i = 1; i < questions_list_array.length+1; i++) {
+      console.log("question"+i);
+      var questionInArray = document.getElementById("STAI_question"+i);
+      questionInArray.style.color = 'black'
+    }
+    updateMainMenu(4);
+}
+
+//if participant does not answer all questions/skips question(s)
+else if (Object.keys(STAI_vals).length < 20) {
+  alert("Please answer all questions.");
+  const questions_list_array = document.getElementsByClassName("STAI_question");
+      for (let i = 1; i < questions_list_array.length+1; i++) {
+        console.log("question"+i);
+        var questionInArray = document.getElementById("STAI_question"+i);
+        if (("question"+i in STAI_vals) == false) {
+          questionInArray.style.color = 'red'
+        }
+        else if (("question"+i in STAI_vals) == true) {
+          questionInArray.style.color = 'black'
+        }
+
+      }
+}
+});
 
 function demoChange(event){
   if (document.getElementById("gender").value != "Blank") {
