c236536df9e16ac279876d8cc88286b1a5463435
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-01-05 18:15:07 -0500

    updated data logging
    fixed number of data points, added falseAlarm to illegalTransitionData, small changes to logOddOneOutData

--- v1/js/task/dataLogging.js
+++ v1/js/task/dataLogging.js
@@ -2,8 +2,8 @@ let nGeneralDataPoints = 12
 let nSectionDataPoints = 4
 let nDragTaskDropDataPoints = 12
 let nDragTaskCheckAnswerDataPoints = 50
-let nIllegalTransitionDataPoints = 11
-let nOddOneOutDataPoints = 24
+let nIllegalTransitionDataPoints = 12
+let nOddOneOutDataPoints = 16
 let generalData, sectionData, dragTaskDropData, dragTaskCheckAnswerData, illegalTransitionData, oddOneOutData;
 
 function dataOrder(){
@@ -35,12 +35,12 @@ function logSectionData(){
 
   data.push(dataOrder())
   console.log(data);
-  //console.log('generalData-logSectionData', generalData.length)
-  //console.log('sectionData-logSectionData', sectionData.length)
-  //console.log('dragTaskDropData-logSectionData', dragTaskDropData.length)
-  //console.log('dragTaskCheckAnswerData-logSectionData', dragTaskCheckAnswerData.length)
-  //console.log('illegalTransitionData-logSectionData', illegalTransitionData.length)
-  // console.log('oddOneOutData-logSectionData', oddOneOutData.length)
+  //console.log('generalData-logSectionData', generalData.length, generalData)
+  //console.log('sectionData-logSectionData', sectionData.length, sectionData)
+  //console.log('dragTaskDropData-logSectionData', dragTaskDropData.length, dragTaskDropData)
+  //console.log('dragTaskCheckAnswerData-logSectionData', dragTaskCheckAnswerData.length, dragTaskCheckAnswerData)
+  //console.log('illegalTransitionData-logSectionData', illegalTransitionData.length, illegalTransitionData)
+  //console.log('oddOneOutData-logSectionData', oddOneOutData.length, oddOneOutData)
 
 
 }
@@ -61,12 +61,12 @@ function logDragDropEvent(dropEvent){
 
   data.push(dataOrder())
   console.log(data);
-  //console.log('generalData-logDragDropEvent', generalData.length)
-  //console.log('sectionData-logDragDropEvent', sectionData.length)
-  //console.log('dragTaskDropData-logDragDropEvent', dragTaskDropData.length)
-  //console.log('dragTaskCheckAnswerData-logDragDropEvent', dragTaskCheckAnswerData.length)
-  //console.log('illegalTransitionData-logDragDropEvent', illegalTransitionData.length)
-  // console.log('oddOneOutData-logDragDropEvent', oddOneOutData.length)
+  //console.log('generalData-logDragDropEvent', generalData.length, generalData)
+  //console.log('sectionData-logDragDropEvent', sectionData.length, sectionData)
+  //console.log('dragTaskDropData-logDragDropEvent', dragTaskDropData.length, dragTaskDropData)
+  //console.log('dragTaskCheckAnswerData-logDragDropEvent', dragTaskCheckAnswerData.length, dragTaskCheckAnswerData)
+  //console.log('illegalTransitionData-logDragDropEvent', illegalTransitionData.length, illegalTransitionData)
+  //console.log('oddOneOutData-logDragDropEvent', oddOneOutData.length, oddOneOutData)
 }
 
 function logDragTaskCheckAnswerData(nCorrect, slotDict){
@@ -85,12 +85,12 @@ function logDragTaskCheckAnswerData(nCorrect, slotDict){
 
   data.push(dataOrder())
   console.log(data);
-  //console.log('generalData-logDragCheckAnswerData', generalData.length)
-  //console.log('sectionData-logDragCheckAnswerData', sectionData.length)
-  //console.log('dragTaskDropData-logDragTaskCheckAnswerData', dragTaskDropData.length)
-  //console.log('dragTaskCheckAnswerData-logDragTaskCheckAnswerData', dragTaskCheckAnswerData.length)
-  //console.log('illegalTransitionData-logDragTaskCheckAnswerData', illegalTransitionData.length)
-  // console.log('oddOneOutData-logDragTaskCheckAnswerData', oddOneOutData.length)
+  //console.log('generalData-logDragCheckAnswerData', generalData.length, generalData)
+  //console.log('sectionData-logDragCheckAnswerData', sectionData.length, sectionData)
+  //console.log('dragTaskDropData-logDragTaskCheckAnswerData', dragTaskDropData.length, dragTaskDropData)
+  //console.log('dragTaskCheckAnswerData-logDragTaskCheckAnswerData', dragTaskCheckAnswerData.length, dragTaskCheckAnswerData)
+  //console.log('illegalTransitionData-logDragTaskCheckAnswerData', illegalTransitionData.length, illegalTransitionData)
+  //console.log('oddOneOutData-logDragTaskCheckAnswerData', oddOneOutData.length, oddOneOutData)
 }
 
 // [sectionType, taskName, trialCount, blockTrialCount, block, trialAttempt, stimOnset, respOnset, respOnset - stimOnset, acc, nCorrect, partResp]
@@ -104,22 +104,22 @@ function logIllegalTransitionData(){
 
   dragTaskCheckAnswerData = new Array(nDragTaskCheckAnswerDataPoints).fill(NaN)
 
-  illegalTransitionData = [fileOnly(activeNode.img.src), activeNode.name, activeNode.index, activeNode.communityNumber, activeNode.community, activeNode.isBoundaryNode ? "b" : "i", transitionType, isCommunityTransition() ? 1 : 0, missedSkip ? 0 : 1, activeNode.threat ? "threat" : "neutral", transitionThreatKind()]
+  illegalTransitionData = [fileOnly(activeNode.img.src), activeNode.name, activeNode.index, activeNode.communityNumber, activeNode.community, activeNode.isBoundaryNode ? "b" : "i", transitionType, isCommunityTransition() ? 1 : 0, missedSkip ? 0 : 1, falseAlarm ? 0 : 1, activeNode.threat ? "threat" : "neutral", transitionThreatKind()]
 
   oddOneOutData = new Array(nOddOneOutDataPoints).fill(NaN)
 
   data.push(dataOrder())
   console.log(data);
-  //console.log('generalData-logIllegalTransitionData', generalData.length)
-  //console.log('sectionData-logIllegalTransitionData', sectionData.length)
-  //console.log('dragTaskDropData-logIllegalTransitionData', dragTaskDropData.length)
-  //console.log('dragTaskCheckAnswerData-logIllegalTransitionData', dragTaskCheckAnswerData.length)
-  //console.log('illegalTransitionData-logIllegalTransitionData', illegalTransitionData.length)
-  // console.log('oddOneOutData-logIllegalTransitionData', oddOneOutData.length)
+  //console.log('generalData-logIllegalTransitionData', generalData.length, generalData)
+  //console.log('sectionData-logIllegalTransitionData', sectionData.length, sectionData)
+  //console.log('dragTaskDropData-logIllegalTransitionData', dragTaskDropData.length, dragTaskDropData)
+  //console.log('dragTaskCheckAnswerData-logIllegalTransitionData', dragTaskCheckAnswerData.length, dragTaskCheckAnswerData)
+  //console.log('illegalTransitionData-logIllegalTransitionData', illegalTransitionData.length, illegalTransitionData)
+  //console.log('oddOneOutData-logIllegalTransitionData', oddOneOutData.length, oddOneOutData)
 }
 
 function logOddOneOutData(currentNodeSet, imageNum){
-  generalData = [sectionType, taskName, nodeSetIterator + 1, nodeSetIterator + 1, block, NaN, stimOnset, respOnset, respOnset - stimOnset, acc, NaN, partResp]
+  generalData = [sectionType, taskName, nodeSetIterator + 1, nodeSetIterator + 1, NaN, NaN, stimOnset, respOnset, respOnset - stimOnset, acc, NaN, partResp]
 
   sectionData = new Array(nSectionDataPoints).fill(NaN)
 
@@ -129,14 +129,14 @@ function logOddOneOutData(currentNodeSet, imageNum){
 
   illegalTransitionData = new Array(nIllegalTransitionDataPoints).fill(NaN)
 
-  oddOneOutData = [currentNodeSet.nodes[imageNum - 1].name, fileOnly(currentNodeSet.nodes[imageNum - 1].img.src), currentNodeSet.nodes[imageNum - 1].communityNumber, currentNodeSet.nodes[0].name, fileOnly(currentNodeSet.nodes[0].img.src), currentNodeSet.nodes[0].communityNumber, currentNodeSet.nodes[1].name,  fileOnly(currentNodeSet.nodes[1].img.src), currentNodeSet.nodes[1].communityNumber, currentNodeSet.nodes[2].name,  fileOnly(currentNodeSet.nodes[2].img.src), currentNodeSet.nodes[2].communityNumber, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, currentNodeSet.nodes[imageNum-1].threat ? "threat" : "neutral", currentNodeSet.nodes[0].threat ? "threat" : "neutral", currentNodeSet.nodes[1].threat ? "threat" : "neutral", currentNodeSet.nodes[2].threat ? "threat" : "neutral"]
+  oddOneOutData = [currentNodeSet.nodes[imageNum - 1].name, fileOnly(currentNodeSet.nodes[imageNum - 1].img.src), currentNodeSet.nodes[imageNum - 1].communityNumber, currentNodeSet.nodes[imageNum-1].threat ? "threat" : "neutral", currentNodeSet.nodes[0].name, fileOnly(currentNodeSet.nodes[0].img.src), currentNodeSet.nodes[0].communityNumber, currentNodeSet.nodes[0].threat ? "threat" : "neutral", currentNodeSet.nodes[1].name,  fileOnly(currentNodeSet.nodes[1].img.src), currentNodeSet.nodes[1].communityNumber, currentNodeSet.nodes[1].threat ? "threat" : "neutral", currentNodeSet.nodes[2].name,  fileOnly(currentNodeSet.nodes[2].img.src), currentNodeSet.nodes[2].communityNumber, currentNodeSet.nodes[2].threat ? "threat" : "neutral"]
 
   data.push(dataOrder())
   console.log(data);
-  //console.log('generalData-logOddOneOutData', generalData.length)
-  //console.log('sectionData-logOddOneOutData', sectionData.length)
-  //console.log('dragTaskDropData-logOddOneOutData', dragTaskDropData.length)
-  //console.log('dragTaskCheckAnswerData-logOddOneOutData', dragTaskCheckAnswerData.length)
-  //console.log('illegalTransitionData-logOddOneOutData', illegalTransitionData.length)
-  // console.log('oddOneOutData-logOddOneOutData', oddOneOutData.length)
+  //console.log('generalData-logOddOneOutData', generalData.length, generalData)
+  //console.log('sectionData-logOddOneOutData', sectionData.length, sectionData)
+  //console.log('dragTaskDropData-logOddOneOutData', dragTaskDropData.length, dragTaskDropData)
+  //console.log('dragTaskCheckAnswerData-logOddOneOutData', dragTaskCheckAnswerData.length, dragTaskCheckAnswerData)
+  //console.log('illegalTransitionData-logOddOneOutData', illegalTransitionData.length, illegalTransitionData)
+  //console.log('oddOneOutData-logOddOneOutData', oddOneOutData.length, oddOneOutData)
 }
